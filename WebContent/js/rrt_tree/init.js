3b02da979e3f3bd012e607f5d4c90926088ca47b
hoshito_taira <hoshito_taira@waku-2.com>
2015-01-13 19:48:05 +0900

    出現回数によってtextを装飾するための準備。

--- WebContent/js/rrt_tree/init.js
+++ WebContent/js/rrt_tree/init.js
@@ -1,6 +1,7 @@
 var create_rrt_tree = function() {
 var diameter = 960;
 
+var iii = 0;
 var tree = d3.layout.tree()
     .size([360, diameter / 2 - 120])
     .separation(function(a, b) { return (a.parent == b.parent ? 1 : 2) / a.depth; });
@@ -38,6 +39,22 @@ d3.json("", function(error, root) {
       .attr("dy", ".31em")
       .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
       .attr("transform", function(d) { return d.x < 180 ? "translate(8)" : "rotate(180)translate(-8)"; })
+      .attr("transform", function(d) { return d.x < 180 ? "translate(8)" : "rotate(180)translate(-8)"; })
+      //.attr("class", function(n) {
+    //	if (!n.children) {
+    //		iii++;
+    //		if (iii % 4 == 0) {
+    //			return "bigger";
+    //		} else if (iii % 4 == 1){
+    //			return "smaller";
+    //		}
+    //		//if (n.size == "bigger") {
+    //		//  return "bigger"
+    //		//} else if (n.size == "smaller") {
+    //		//  return "smaller"
+    //		//}
+    //	}
+      //})
       .text(function(d) { return d.name; });
 });
 
