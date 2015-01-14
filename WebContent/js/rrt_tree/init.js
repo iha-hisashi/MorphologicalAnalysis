8711be5e446382db6d2d658bfb28262ab5b82aa5
hoshito-taira <bravebirdn0n0n0@gamil.com>
2015-01-15 00:34:39 +0900

    頻出頻度で装飾

--- WebContent/js/rrt_tree/init.js
+++ WebContent/js/rrt_tree/init.js
@@ -1,7 +1,6 @@
 var create_rrt_tree = function() {
-var diameter = 960;
+var diameter = 1080;
 
-var iii = 0;
 var tree = d3.layout.tree()
     .size([360, diameter / 2 - 120])
     .separation(function(a, b) { return (a.parent == b.parent ? 1 : 2) / a.depth; });
@@ -17,8 +16,34 @@ var svg = d3.select("body").append("svg")
 
 d3.json("", function(error, root) {
   root = local_json;
-  var nodes = tree.nodes(root),
-      links = tree.links(nodes);
+  var nodes = tree.nodes(root).sort(function(n1, n2){
+	  // 末端である -> 頻出降順 -> name辞書昇順 
+	  if ((!('children' in n1)) && ('children' in n2)) {
+		  return -1;
+	  } else if (('children' in n1) && (!('children' in n2))) {
+		  return 1;
+	  } else if (n2.frequent != n1.frequent) {
+		  return n2.frequent - n1.frequent;
+	  } else if (n2.name > n1.name){
+		  return -1;
+	  } else if (n2.name < n1.name) {
+		  return 1;
+	  }
+  });
+  var word_nums = $.grep(nodes, function(node) { return !('children' in node); }).length;
+  // 高レート順位の定義
+  var high_rate_rank = word_nums * 10 / 100;
+  // 低レート順位の定義
+  var low_rate_rank = word_nums * 90 / 100;
+  nodes = $.map(nodes, function(node, index){
+    node.rank = index + 1;
+    // 高レートならtrue
+    node.high_rate = !('children' in node) && node.rank <= high_rate_rank;
+    // 低レートならtrue
+    node.low_rate = !('children' in node) && node.rank > low_rate_rank;
+	return node;
+  });
+  var links = tree.links(nodes);
 
   var link = svg.selectAll(".link")
       .data(links)
@@ -40,21 +65,13 @@ d3.json("", function(error, root) {
       .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
       .attr("transform", function(d) { return d.x < 180 ? "translate(8)" : "rotate(180)translate(-8)"; })
       .attr("transform", function(d) { return d.x < 180 ? "translate(8)" : "rotate(180)translate(-8)"; })
-      //.attr("class", function(n) {
-    //	if (!n.children) {
-    //		iii++;
-    //		if (iii % 4 == 0) {
-    //			return "bigger";
-    //		} else if (iii % 4 == 1){
-    //			return "smaller";
-    //		}
-    //		//if (n.size == "bigger") {
-    //		//  return "bigger"
-    //		//} else if (n.size == "smaller") {
-    //		//  return "smaller"
-    //		//}
-    //	}
-      //})
+      .attr("class", function(n) {
+    	  if (n.high_rate) {
+    		  return "bigger";
+    	  } else if (n.low_rate) {
+    		  return "smaller";
+    	  }
+      })
       .text(function(d) { return d.name; });
 });
 
